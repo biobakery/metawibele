1248fccf3947110ff8dc80dca8cc1e6b4bc00c25
YancongZhang <zhangyc201211@gmail.com>
2020-01-26 17:57:38 -0500

    Add functions for analyzing stratified abundance

---
+++ metawibele/characterize/maaslin2_annotator_stratified.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+"""
+MetaWIBELE: maaslin2_annotator module
+Summary maaslin2 annotation
+
+Copyright (c) 2019 Harvard School of Public Health
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import sys
+import os
+import os.path
+import re
+import argparse
+import math
+
+try:
+	from metawibele import config
+	from metawibele import utilities
+except ImportError:
+	sys.exit("CRITICAL ERROR: Unable to find the MetaWIBELE python package." +
+	         " Please check your install.")
+
+# ---------------------------------------------------------------
+# Description and arguments
+# ---------------------------------------------------------------
+description = """
+Summary association annotation for protein families
+"""
+
+def get_args():
+
+	parser=argparse.ArgumentParser()
+	parser.add_argument('-s', "--stat",
+	                    help='input statistical summary file',
+	                    required=True)
+	parser.add_argument('-t', "--type",
+	                    help='specify the type of abundance table, e.g. MaAsLin2_DA',
+	                    required=True)
+	parser.add_argument('-e', "--effect-size",
+	                    help='specify the name indication effect size, e.g. coef | log(FC) | mean(log)',
+	                    required=True)
+	parser.add_argument('-o', "--output",
+	                    help='output annotated file',
+	                    required=True)
+	values = parser.parse_args()
+
+	return values
+# get_args
+
+
+#==============================================================
+# get annotation about differential abundance info
+#==============================================================
+def stat_annotation (statfile, mytype, effect_name, outfile):
+	titles = {}
+	stat = []
+	open_file = open(statfile, "r")
+	open_out = open(outfile, "w")
+	for line in open_file:
+		line = line.strip()
+		if not len(line):
+			continue
+		info = line.split("\t")
+		if re.search("^" + utilities.PROTEIN_FAMILY_ID, line):
+			for item in info:
+				titles[item] = info.index(item)
+			mytitle = "\t".join(info[1:])
+			open_out.write(utilities.PROTEIN_FAMILY_ID + "\ttype\tdetail\t" + mytitle + "\n")
+			continue
+		myid = info[titles[utilities.PROTEIN_FAMILY_ID]]
+		myid = myid.split("|")[0]
+		myeffect = info[titles["coef"]]
+		if effect_name in titles:
+			myeffect = info[titles[effect_name]]
+		mycmp = "NA"
+		if "cmp_type" in titles:
+			mycmp = info[titles["cmp_type"]]
+		mydetail = "NA"
+		if myeffect != "NA" and myeffect != "NaN" and myeffect != "nan":
+			if float(myeffect) < 0:
+				mydetail = "down"
+			else:
+				if float(myeffect) > 0:
+					mydetail = "up"
+				else:
+					mydetail = "NA"
+		if mycmp != "NA":
+			mydetail = mydetail + "(" + mycmp + ")"
+		mystat = "\t".join(info[1:])
+		open_out.write (myid + "\t" + mytype + "\t" + mydetail + "\t" + mystat + "\n")
+	# foreach line
+	open_file.close()
+	open_out.close()
+
+# func: stat_annotation
+
+
+
+#==============================================================
+###########  Main processing ############
+#==============================================================
+if __name__ == '__main__':
+	
+	### get arguments ###
+	values = get_args()
+
+	sys.stderr.write("### Start maaslin2_annotator.py -s " + values.stat + " ####\n")
+
+	### get info
+	sys.stderr.write("\nGet DA annotation info ......starting\n")
+	stat_annotation (values.stat, values.type, values.effect_size, values.output)
+	sys.stderr.write("Get DA annotation info ......done\n")
+
+	sys.stderr.write("### Finish maaslin2_annotator.py ####\n\n\n")
+
+# end: main
--- metawibele/common/rna_stratified_abundance_filtering.py
+++ metawibele/common/rna_stratified_abundance_filtering.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-MetaWIBELE: sum_to_protein_family_stratified_abundance module
-Sum over stratified protein families based on reads mapped to gene catalog of nucleotide representatives
+MetaWIBELE: rna_stratified_abundance_filtering module
+have a threshold for MSP presence (e.g. >X% of genes non-zero in RNA) per sample
 
 Copyright (c) 2019 Harvard School of Public Health
 
@@ -49,11 +49,9 @@ Sum over protein families based on reads mapped to gene catalog of nucleotide re
 
 def get_args ():	
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-i', help='input abundance file', required=True)
-	parser.add_argument('-m', help='input taxonomy file', required=True)
-	parser.add_argument('-r', help='specify taxonomic level, e.g. Species | MSP', default="Species")
-	parser.add_argument('-t', help='threshold for taxon presence (e.g. >X% of genes non-zero in RNA)', default=0.5)
-	parser.add_argument('-o', help='output stratified abundance file', required=True)
+	parser.add_argument('-i', help='input normalized abundance', required=True)
+	parser.add_argument('-t', help='threshold for taxon presence (e.g. >X% of genes non-zero in RNA)', default=0.25)
+	parser.add_argument('-o', help='output refined stratified abundance file', required=True)
 	values = parser.parse_args()
 	return values
 # get_args
@@ -90,7 +88,8 @@ def collect_protein_cluster_info (clust_file):
 #==============================================================
 # collect taxa info
 #==============================================================
-def collect_taxa_info (taxa_file, taxa_level):
+def collect_taxa_info (taxa_file):
+	taxa_levels = ["Species"]
 	taxa = {}
 	taxa_num = {}
 	titles = {}
@@ -108,17 +107,17 @@ def collect_taxa_info (taxa_file, taxa_level):
 		myid = info[0]
 		mytaxa = "NA"
 		mylevel = "NA"
-		if taxa_level == "MSP":
-			if "msp_name" in titles:
-				mytaxa = info[titles["msp_name"]]
-		else:		
+		if "taxa_name" in titles:
+			mytaxa = info[titles["taxa_name"]]
 			if "taxa_rank" in titles:
 				 mylevel = info[titles["taxa_rank"]]
-			if mylevel != taxa_level:
+		else:
+			if "detail" in titles:
+				mytaxa = info[titles["detail"]]
+		if mylevel != "NA":
+			if not mylevel in taxa_levels:
 				continue
-			if "taxa_name" in titles:
-				mytaxa = info[titles["taxa_name"]]
-		if mytaxa == "NA" or mytaxa == "Unclassified" or mytaxa == "msp_unknown":
+		if mytaxa == "NA" or mytaxa == "Unclassified":
 			continue
 		taxa[myid] = mytaxa
 		if not mytaxa in taxa_num:
@@ -132,12 +131,13 @@ def collect_taxa_info (taxa_file, taxa_level):
 
 
 #==============================================================
-# sum up to family abundance
+# filter genes without species existing
 #==============================================================
-def sum_abundance (pep_cluster, taxa, taxa_num, flt_presence, infile, outfile):
+def filter_abundance (flt_presence, infile, outfile):
 	titles = {}
 	samples = []
 	taxa_presence = {}
+	taxa_num = {}
 	outs = {}
 	open_file = open(infile, "r")
 	line = open_file.readline()
@@ -155,66 +155,73 @@ def sum_abundance (pep_cluster, taxa, taxa_num, flt_presence, infile, outfile):
 		if not len(line):
 			continue
 		info = line.split("\t")
-		myid = info[0]
-		if not myid in pep_cluster:
-			continue
-		if myid in taxa:
-			mytaxa = taxa[myid]
-		else:
-			continue
-		myclust = pep_cluster[myid] + "|" + mytaxa
-		if not mytaxa in taxa_presence:
-			taxa_presence[mytaxa] = {}
-		taxa_presence[mytaxa][myid] = ""
+		myclust = info[0]
+		myid = myclust.split("|")[0]
+		mytaxa = myclust.split("|")[-1]
+		if not mytaxa in taxa_num:
+			taxa_num[mytaxa] = {}
+		taxa_num[mytaxa][myid] = ""
 		if not myclust in outs:
 			outs[myclust] = {}
 		myindex = 1
 		while myindex < len(info):
 			mycount = info[myindex]
 			mys = titles[myindex]
-			if not mys in outs[myclust]:
-				outs[myclust][mys] = 0
-			if re.search("\.", mycount):
-				outs[myclust][mys] = outs[myclust][mys] + float(mycount)
-			else:
-				outs[myclust][mys] = outs[myclust][mys] + int(mycount)
+			if float(mycount) != 0:
+				if not mytaxa in taxa_presence:
+					taxa_presence[mytaxa] = {}
+				if not mys in taxa_presence[mytaxa]:
+					taxa_presence[mytaxa][mys] = {}
+				taxa_presence[mytaxa][mys][myclust] = ""
 			myindex = myindex + 1
 		# foreach sample
 	# foreach line
 	open_file.close()
 
 	# threshold for species presence (e.g. >X% of genes non-zero in RNA)
-	refined_taxa = {}
+	flt_taxa = {}
 	for mytaxa in taxa_presence:
-		mynum = len(taxa_presence[mytaxa].keys())
-		if mytaxa in taxa_num:
-			mytotal = len(taxa_num[mytaxa].keys())
-			myper = float(mynum) / float(mytotal)
-			if myper >= float(flt_presence):
-				refined_taxa[mytaxa] = ""
+		for mys in taxa_presence[mytaxa].keys():
+			mynum = len(taxa_presence[mytaxa][mys].keys())
+			if mytaxa in taxa_num:
+				mytotal = len(taxa_num[mytaxa].keys())
+				myper = float(mynum) / float(mytotal)
+				if myper < float(flt_presence):
+					if not mytaxa in flt_taxa:
+						flt_taxa[mytaxa] = {}
+					flt_taxa[mytaxa][mys] = ""
 
 	# output abundance
-	samples = list(set(samples)) 
+	open_file = open(infile, "r")
+	line = open_file.readline()
 	open_out = open(outfile, "w")
-	mytitle = "ID\t" + "\t".join(samples)
-	open_out.write(mytitle + "\n")
-	for myclust in sorted(outs.keys()):
-		mystr = "Cluster_" + myclust
-		tmp = myclust.split("|")
-		if not tmp[1] in refined_taxa:
+	open_out.write(line)
+	for line in open_file:
+		line = line.strip()
+		if not len(line):
 			continue
-		for mys in samples:
-			mycount = 0
-			if mys in outs[myclust]:
-				mycount = outs[myclust][mys]
-				#for myid in outs[myclust][mys]:
-				#	mycount = mycount + outs[myclust][mys]
-			mystr = mystr + "\t" + str(mycount)
-		# foreach samples
+		info = line.split("\t")
+		myclust = info[0]
+		myid = myclust.split("|")[0]
+		mytaxa = myclust.split("|")[-1]
+		mystr = myclust
+		myindex = 1
+		while myindex < len(info):
+			mycount = info[myindex]
+			mys = titles[myindex]
+			flag = 0
+			if mytaxa in flt_taxa:
+				if mys in flt_taxa[mytaxa]:
+					mystr = mystr + "\tNaN"
+					flag = 1
+			if flag == 0:
+				mystr = mystr + "\t" + mycount
+			myindex = myindex + 1
+		# foreach sample
 		open_out.write(mystr + "\n")
 	# foreach protein family
 	open_out.close()
-# assign_counts
+# filter_abundance
 
 
 #==============================================================
@@ -226,21 +233,17 @@ def main():
 	values = get_args()
 
 
-	sys.stderr.write("### Start sum_to_protein_family_stratified_abundance.py -i " + values.i + " ####\n")
+	sys.stderr.write("### Start rna_stratified_abundance_filtering.py -i " + values.i + " ####\n")
 	
 
 	### collect cluster info ###
-	sys.stderr.write("Get cluster info ......starting\n")
-	pep_cluster = collect_protein_cluster_info (config.protein_family)
-	taxa, taxa_num = collect_taxa_info (values.m, values.r)
-	sys.stderr.write("Get cluster info ......done\n")
 
-	### sum abundance to protein families ###
-	sys.stderr.write("\nAssign counts to protein families ......starting\n")
-	sum_abundance (pep_cluster, taxa, taxa_num, values.t, values.i, values.o)
-	sys.stderr.write("\nAssign counts to protein families ......done\n")
+	### filter abundance ###
+	sys.stderr.write("\nFilter abundance for rotein families ......starting\n")
+	filter_abundance (values.t, values.i, values.o)
+	sys.stderr.write("\nFilter abundance for protein families ......done\n")
 
-	sys.stderr.write("### Finish sum_to_protein_family_stratified_abundance.py ####\n\n\n")
+	sys.stderr.write("### Finish rna_stratified_abundance_filtering.py ####\n\n\n")
 
 # end: main
 
